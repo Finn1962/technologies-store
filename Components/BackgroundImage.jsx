3d65efdc1c85b842b163ece60e194c536e54efad
Finn Schmidt <finn.schmidt@brainworxx.de>
2026-05-22 16:11:08 +0200

    Added a Text and Respnsivity

--- Components/BackgroundImage.jsx
+++ Components/BackgroundImage.jsx
@@ -2,11 +2,25 @@ import backgroundImage from "../assets/Background.jpg";
 import styled from "styled-components";
 
 export default function BackgroundImage() {
-  return <StyledBackgroundImage src={backgroundImage} alt="Background" />;
+  return (
+    <>
+      <StyledOverlay />
+      <StyledBackgroundImage src={backgroundImage} alt="Background" />
+    </>
+  );
 }
+
+const StyledOverlay = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  color: black;
+  z-index: 10;
+`;
 
 const StyledBackgroundImage = styled.img`
   width: 100%;
   height: 100vh;
   object-fit: cover;
+  z-index: 1;
 `;
--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -2,12 +2,39 @@ import BackgroundImage from "../Components/BackgroundImage.jsx";
 import MacbookFront from "../Components/MacbookFront.jsx";
 import MacbookBack from "../Components/MacbookBack.jsx";
 
+import styled from "styled-components";
+
 export default function Home() {
   return (
-    <main>
+    <PageContainer>
+      <p className="text">
+        Dein Onlinestore für <br></br> Technik, die begeistert.
+      </p>
       <MacbookFront />
       <MacbookBack />
       <BackgroundImage />
-    </main>
+    </PageContainer>
   );
 }
+
+const PageContainer = styled.div`
+  position: Absolute;
+  width: 100%;
+  height: 100%;
+
+  .text {
+    position: absolute;
+    color: white;
+    font-size: 3rem;
+    font-weight: 200;
+    z-index: 3;
+    left: 3%;
+    bottom: 10%;
+  }
+
+  @media (max-width: 768px) {
+    .text {
+      font-size: 2rem;
+    }
+  }
+`;
